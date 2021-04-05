e9506ca89978921c19e12efb30a6172823ac97d4
Wonski <krzysztof.wilewski@gmail.com>
2021-04-05 20:01:02 +0200

    db bugged sth works

--- app/src/main/java/com/narrowstudio/bigbrainz/viewmodel/Game2ViewModel.kt
+++ app/src/main/java/com/narrowstudio/bigbrainz/viewmodel/Game2ViewModel.kt
@@ -34,8 +34,6 @@ class Game2ViewModel @ViewModelInject constructor(
 
     val saves = g2Dao.getEntries().asLiveData()
 
-    var totalAverage: MutableLiveData<Long> = MutableLiveData()
-
 
 
 
@@ -69,7 +67,6 @@ class Game2ViewModel @ViewModelInject constructor(
         shouldGameBeRestarted.postValue(false)
         gameState.postValue(0)
         resetTimeArray()
-        calculateTotalAverage()
     }
 
     fun buttonPressed(){
@@ -97,21 +94,6 @@ class Game2ViewModel @ViewModelInject constructor(
 
     }
 
-    private fun calculateTotalAverage(){
-        if (saves.value?.size != 0 || saves.value?.size != null){
-            var sum: Long = 0
-            val entries: Int? = saves.value?.size
-
-            //summing total average time of saved entries
-            /*for (i in 0 until (entries!!-1)){
-                sum += saves.value!![i].averageTime
-            }*/
-            totalAverage.postValue(saves.value?.size?.toLong())
-        } else {
-            totalAverage.postValue(0)
-        }
-    }
-
     private fun handleTimeArray(){
         // first add time to the array, then process
         timeArray.add(millisecondTime)
@@ -127,7 +109,6 @@ class Game2ViewModel @ViewModelInject constructor(
             averageTime.postValue(average)
             insertNewEntry()
             resetTimeArray()
-            calculateTotalAverage()
         }
 
     }
