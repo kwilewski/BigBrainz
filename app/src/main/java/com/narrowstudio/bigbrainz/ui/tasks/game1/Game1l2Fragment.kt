fa986599657a78fed3f95cd2cb9b3449cf3a2f0b
kwilewski <37414509+kwilewski@users.noreply.github.com>
2023-10-09 20:05:01 +0200

    g1l2 started

---
+++ app/src/main/java/com/narrowstudio/bigbrainz/ui/tasks/game1/Game1l2Fragment.kt
@@ -0,0 +1,181 @@
+package com.narrowstudio.bigbrainz.ui.tasks.game1
+
+import android.os.Bundle
+import android.util.TypedValue
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Observer
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import com.narrowstudio.bigbrainz.R
+import com.narrowstudio.bigbrainz.databinding.FragmentGame1L1Binding
+import com.narrowstudio.bigbrainz.viewmodel.game1.Game1l2ViewModel
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class Game1l2Fragment : Fragment(R.layout.fragment_game_1_l_2), LifecycleOwner {
+
+    private val g1l2ViewModel: Game1l2ViewModel by viewModels()
+
+    // opening score
+    private lateinit var openScore: LiveData<Boolean>
+
+    var navController : NavController? = null
+
+    // View Binding
+    private var _binding: FragmentGame1L1Binding? = null
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentGame1L1Binding.inflate(inflater, container, false)
+        val view = binding.root
+
+        navController = Navigation.findNavController(requireActivity(), R.id.nav_host_fragment)
+
+        g1l2ViewModel.init()
+
+        binding.g1DisplayButton.setOnTouchListener(View.OnTouchListener{v, event ->
+            g1l2ViewModel.mainButtonPressed()
+            true
+        })
+
+        g1l2ViewModel.gameState.observe(viewLifecycleOwner, Observer{
+            buttonColorHandler()
+            buttonIconHandler()
+            buttonVisibilityHandler()
+            topTextHandler()
+            bottomTextHandler()
+        })
+
+        g1l2ViewModel.colorPressedLD.observe(viewLifecycleOwner, Observer {
+            topTextHandler()
+        })
+
+        //opening score fragment
+        openScore = g1l2ViewModel.openScore
+        openScore.observe(viewLifecycleOwner, Observer{
+            openScoreFragment()
+        })
+
+        binding.g1Color1Button.setOnClickListener(View.OnClickListener {v ->
+            g1l2ViewModel.colorButtonPressed(0)
+        })
+        binding.g1Color2Button.setOnClickListener(View.OnClickListener {v ->
+            g1l2ViewModel.colorButtonPressed(1)
+        })
+        binding.g1Color3Button.setOnClickListener(View.OnClickListener {v ->
+            g1l2ViewModel.colorButtonPressed(2)
+        })
+        binding.g1Color4Button.setOnClickListener(View.OnClickListener {v ->
+            g1l2ViewModel.colorButtonPressed(3)
+        })
+
+
+        return view
+    }
+
+    private fun topTextHandler(){
+        when (g1l2ViewModel.gameState.value){
+            0, 2 -> {
+                binding.g1l1TopTextView.text = getString(R.string.g1l1_remaining,
+                    g1l2ViewModel.colorsToBeShown - g1l2ViewModel.counter)
+            }
+            3 -> {
+                binding.g1l1TopTextView.text = getString(R.string.g1l1_remaining,
+                    g1l2ViewModel.colorsToBeShown - g1l2ViewModel.inputArray.size)
+            }
+            4 -> {
+                binding.g1l1TopTextView.text = ""
+            }
+        }
+    }
+
+    private fun bottomTextHandler(){
+        when (g1l2ViewModel.gameState.value){
+            0, 1, 2, 3 -> {
+                binding.g1Textview.text = getString(R.string.g1l1_info,
+                    g1l2ViewModel.colorsToBeShown)
+            }
+        }
+    }
+
+
+    private fun buttonColorHandler(){
+        when (g1l2ViewModel.buttonColor){
+            0 -> {
+                binding.g1DisplayButton.setBackgroundColor(ContextCompat.getColor(this.requireContext(), R.color.g1Red))
+            }
+            1 -> {
+                binding.g1DisplayButton.setBackgroundColor(ContextCompat.getColor(this.requireContext(), R.color.g1Green))
+            }
+            2 -> {
+                binding.g1DisplayButton.setBackgroundColor(ContextCompat.getColor(this.requireContext(), R.color.g1Blue))
+            }
+            3 -> {
+                binding.g1DisplayButton.setBackgroundColor(ContextCompat.getColor(this.requireContext(), R.color.g1Yellow))
+            }
+            4 -> {
+                val typedValue = TypedValue()
+                requireActivity().theme.resolveAttribute(R.attr.colorButtonWaiting, typedValue, true)
+                if (typedValue.resourceId != 0) {
+                    binding.g1DisplayButton.setBackgroundResource(typedValue.resourceId)
+                } else {
+                    // this should work whether there was a resource id or not
+                    binding.g1DisplayButton.setBackgroundResource(typedValue.data)
+                }
+            }
+        }
+    }
+
+    private fun buttonIconHandler(){
+        when (g1l2ViewModel.gameState.value){
+            0 -> {
+                binding.g1DisplayButton.setImageResource(R.drawable.ic_baseline_touch)
+            }
+            else -> {
+                binding.g1DisplayButton.setImageDrawable(null)
+            }
+        }
+    }
+
+    private fun buttonVisibilityHandler(){
+        when (g1l2ViewModel.gameState.value){
+            0, 1, 2, 4 -> {
+                binding.g1ColorDisplay.visibility = View.VISIBLE
+                binding.g1ButtonsGrid.visibility = View.INVISIBLE
+            }
+            3 -> {
+                binding.g1ColorDisplay.visibility = View.INVISIBLE
+                binding.g1ButtonsGrid.visibility = View.VISIBLE
+            }
+        }
+    }
+
+    private fun openScoreFragment(){
+        if (openScore.value == true) {
+            navController!!.navigate(R.id.action_game1l1Fragment_to_game1l1ScoreFragment)
+        }
+    }
+
+
+
+}
---
+++ app/src/main/java/com/narrowstudio/bigbrainz/viewmodel/game1/Game1l2ViewModel.kt
@@ -0,0 +1,258 @@
+package com.narrowstudio.bigbrainz.viewmodel.game1
+
+import android.os.Handler
+import android.os.Looper
+import android.util.Log
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
+import com.narrowstudio.bigbrainz.data.G1DBEntry
+import com.narrowstudio.bigbrainz.data.G1Dao
+import com.narrowstudio.bigbrainz.data.G2DBEntry
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.*
+import javax.inject.Inject
+import kotlin.random.Random
+
+@HiltViewModel
+class Game1l2ViewModel @Inject constructor(
+    private val g1Dao: G1Dao
+) : ViewModel() {
+
+    /**    ---------------------- gameState ----------------------------------
+     *      0 - not running
+     *      1 - displaying the color
+     *      2 - displaying intercolor
+     *      3 - reading the input
+     *      4 - displaying wrong
+     */
+    val gameState: MutableLiveData<Int> = MutableLiveData()
+
+    // time of starting the timer in millis
+    private var startTime: Long = 0
+
+    /**     ---------------------- buttonColor ----------------------------------
+     *      0 - Red
+     *      1 - Green
+     *      2 - Blue
+     *      3 - Yellow
+     *      4 - Grey
+     */
+    // list of colors received from fragment
+    val colorList: ArrayList<Int> = ArrayList()
+
+    // array with indexes of generated colors
+    private var colorArray: ArrayList<Int> = ArrayList()
+
+    var inputArray: ArrayList<Int> = ArrayList()
+
+    val colorPressedLD:MutableLiveData<Boolean> = MutableLiveData()
+
+    // sends info to fragment to open score fragment
+    var openScore: MutableLiveData<Boolean> = MutableLiveData()
+
+    var buttonColor: Int = 4
+
+    // length of the first challenge
+    private val colorsAtTheBeginning = 3
+    // length of level
+    var colorsToBeShown = colorsAtTheBeginning
+    // counter of already shown colors
+    var counter = 0
+    // increment of length at each level
+    private val levelUp = 1
+
+    // time for each color in millis
+    private val displayTime = 600
+
+    // time for intercolor in millis
+    private val interColorTIme = 250
+
+    // counting red screens after wrong click
+    private var wrongColorCounter = 0
+    private var wrongColorMaxCount = 3
+
+    // time of displaying red after wrong click
+    private val wrongColorRedTime = 300
+    private val wrongColorGreyTime = 150
+
+    val saves = g1Dao.getEntries().asLiveData()
+
+
+    private var handler = Handler(Looper.getMainLooper())
+
+    private val scope = CoroutineScope(Dispatchers.Main)
+
+    private var runnable: java.lang.Runnable = object: java.lang.Runnable {
+        override fun run(){
+            when (gameState.value){
+                1 -> {
+                    if (System.currentTimeMillis() >= startTime + displayTime){
+                        if (counter < colorsToBeShown) {
+                            colorToInter()
+                        } else {
+                            colorToInput()
+                        }
+                    }
+                }
+                2 -> {
+                    if (System.currentTimeMillis() >= startTime + interColorTIme){
+                        interToColor()
+                    }
+                }
+                4 -> { // timer for displaying red flashed
+                    when (buttonColor){
+                        0 -> {
+                            if (System.currentTimeMillis() >= startTime + wrongColorRedTime){
+                                wrongRed()
+                            }
+                        }
+                        4 -> {
+                            if (System.currentTimeMillis() >= startTime + wrongColorGreyTime){
+                                wrongGrey()
+                            }
+                        }
+                    }
+
+                }
+            }
+
+            handler.postDelayed(this, 1)
+        }
+    }
+
+
+
+
+    fun init(){
+        stopTimer()
+        buttonColor = 4
+        colorArray.clear()
+        inputArray.clear()
+        openScore.postValue(false)
+        counter = 0
+        wrongColorCounter = 0
+    }
+
+    fun mainButtonPressed(){
+        when (gameState.value){
+            0 -> {
+                startGame()
+            }
+            1 -> {
+                // do nothing
+            }
+            2 -> {
+                // do nothing
+            }
+            3 -> {
+                // do nothing
+            }
+            4 -> {
+                // do nothing
+            }
+        }
+    }
+
+    fun colorButtonPressed(color: Int){
+        inputArray.add(color)
+        colorPressedLD.postValue(true)
+        if (color != colorArray[inputArray.size-1]){
+            restartGame()
+            return
+        }
+        if (inputArray.size == colorsToBeShown){
+            nextLevel()
+            return
+        }
+
+    }
+
+    private fun startGame(){
+        Log.d("Game status", "Game started")
+        interToColor()
+        startTimer()
+    }
+
+    private fun restartGame(){
+        // if any successful pattern, save
+        if (colorsToBeShown == colorsAtTheBeginning) {
+            gameState.postValue(4)
+            colorsToBeShown = colorsAtTheBeginning
+            buttonColor = 4
+            startTime = System.currentTimeMillis()
+            startTimer()
+        } else {
+            // if any successful pattern, open score
+            insertNewEntry()
+            openScore.postValue(true)
+        }
+    }
+
+    private fun nextLevel(){
+        colorsToBeShown += levelUp
+        Log.d("Game status", "Level up. Current level: $colorsToBeShown")
+        init()
+    }
+
+    // transition from state 1 to 2
+    private fun colorToInter(){
+        buttonColor = 4
+        gameState.postValue(2)
+        startTime = System.currentTimeMillis()
+    }
+
+    // transition from state 1 to 3
+    private fun colorToInput(){
+        stopTimer()
+        gameState.postValue(3)
+    }
+
+    // transition from state 2 to 1
+    private fun interToColor(){
+        counter++
+        colorArray.add(randomizeColor())
+        buttonColor = colorArray.last()
+        gameState.postValue(1)
+        startTime = System.currentTimeMillis()
+    }
+
+    private fun wrongRed(){
+        if (++wrongColorCounter < wrongColorMaxCount){
+            buttonColor = 4
+            startTime = System.currentTimeMillis()
+            gameState.postValue(4)
+        } else {
+            stopTimer()
+            init()
+        }
+    }
+
+    private fun wrongGrey(){
+        startTime = System.currentTimeMillis()
+        buttonColor = 0
+        gameState.postValue(4)
+    }
+
+    private fun randomizeColor(): Int{
+        return (0..3).random(Random(System.currentTimeMillis()))
+    }
+
+    private fun startTimer(){
+        handler.removeCallbacks(runnable)
+        handler.postDelayed(runnable, 1)
+    }
+
+    private fun stopTimer(){
+        handler.removeCallbacks(runnable)
+        gameState.postValue(0)
+    }
+
+    //-------------------------------------------------- DB
+    private fun insertNewEntry() {
+        scope.launch {
+            g1Dao.insert(G1DBEntry(101, colorsToBeShown - 1))
+        }
+    }
+
+}
