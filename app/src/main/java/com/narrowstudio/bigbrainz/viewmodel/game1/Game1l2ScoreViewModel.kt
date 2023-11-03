29750657d414bad38705747aa678240b9dcf0a16
kwilewski <37414509+kwilewski@users.noreply.github.com>
2023-11-03 15:12:14 +0100

    added g1l1 score

---
+++ app/src/main/java/com/narrowstudio/bigbrainz/viewmodel/game1/Game1l2ScoreViewModel.kt
@@ -0,0 +1,69 @@
+package com.narrowstudio.bigbrainz.viewmodel.game1
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
+import com.narrowstudio.bigbrainz.data.G1DBEntry
+import com.narrowstudio.bigbrainz.data.G1Dao
+import com.narrowstudio.bigbrainz.data.G2DBEntry
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import javax.inject.Inject
+
+@HiltViewModel
+class Game1l2ScoreViewModel @Inject constructor(
+    private val g1Dao: G1Dao
+) : ViewModel() {
+    var lastScore: Int = 0
+    var bestScore: Int = 0
+
+    val gameID: Int = 102
+
+    val saves = g1Dao.getEntries().asLiveData()
+    private var savesList: MutableList<G1DBEntry> = mutableListOf()
+
+
+    private val scope = CoroutineScope(Dispatchers.Main)
+
+
+    fun init(){
+        filterEntries()
+        getLastScoreFromDB()
+        getBestScoreFromDB()
+    }
+
+    fun getScoresFromDB(){
+        filterEntries()
+        getLastScoreFromDB()
+        getBestScoreFromDB()
+    }
+
+    private fun filterEntries(){
+        if (saves.value?.size == null) return
+        for (entry in saves.value!!){
+            if (entry.gameID == gameID) {
+                savesList.add(entry)
+            }
+        }
+    }
+
+
+    private fun getLastScoreFromDB(){
+        val entries: Int = savesList.size
+        if (entries != 0) lastScore = savesList[entries - 1].score
+    }
+
+    private fun getBestScoreFromDB(){
+        val entries: Int = savesList.size
+        var best: Int = 0
+        if (entries != 0){
+            best = savesList[0].score
+            for (i in 0 until entries){
+                if (savesList[i].score > best) best = savesList[i].score
+            }
+            bestScore = best
+        }
+    }
+
+
+}
