c942ac9c1d21eaec3be6f7145b4ab32329236ee3
kwilewski <37414509+kwilewski@users.noreply.github.com>
2023-11-08 13:39:43 +0100

    g1l3 files added

---
+++ app/src/main/java/com/narrowstudio/bigbrainz/ui/tasks/game1/Game1l3Fragment.kt
@@ -0,0 +1,221 @@
+package com.narrowstudio.bigbrainz.ui.tasks.game1
+
+import android.content.Context
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.Observer
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import com.narrowstudio.bigbrainz.R
+import com.narrowstudio.bigbrainz.databinding.FragmentGame1L2Binding
+import com.narrowstudio.bigbrainz.viewmodel.game1.Game1l2ViewModel
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class Game1l3Fragment : Fragment(R.layout.fragment_game_1_l_2), LifecycleOwner {
+
+    private val g1l2ViewModel: Game1l2ViewModel by viewModels()
+
+    // opening score
+    private lateinit var openScore: LiveData<Boolean>
+
+    var navController : NavController? = null
+
+    // View Binding
+    private var _binding: FragmentGame1L2Binding? = null
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentGame1L2Binding.inflate(inflater, container, false)
+        val view = binding.root
+
+        navController = Navigation.findNavController(requireActivity(), R.id.nav_host_fragment)
+
+        g1l2ViewModel.init()
+
+        binding.g1l2StartButton.setOnTouchListener(View.OnTouchListener{v, event ->
+            g1l2ViewModel.mainButtonPressed()
+            true
+        })
+
+
+        g1l2ViewModel.gameState.observe(viewLifecycleOwner, Observer{
+            buttonIconHandler()
+            layoutVisibilityHandler()
+            topTextHandler()
+            middleTextHandler()
+            bottomTextHandler()
+        })
+
+
+        //opening score fragment
+        openScore = g1l2ViewModel.openScore
+        openScore.observe(viewLifecycleOwner, Observer{
+            openScoreFragment()
+        })
+
+        // swtting progress bar
+        g1l2ViewModel.progressBar.observe(viewLifecycleOwner, Observer {
+            progressBarHandler()
+        })
+
+        binding.g1l2SequenceButton.setOnClickListener(View.OnClickListener {v ->
+            sequenceButtonClicked()
+        })
+
+
+
+        return view
+    }
+
+    private fun sequenceButtonClicked(){
+        // readind the code from the EditText
+        val code: Long? = binding.g1l2SequenceInput.text.toString().toLongOrNull()
+        if (code != null) {
+            g1l2ViewModel.codeEntered(code)
+        }
+        // removing the code from ET after the click
+        binding.g1l2SequenceInput.setText("")
+
+        // hiding the keyboard after the click
+        val imm = activity?.getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(requireView().windowToken, 0)
+    }
+
+    private fun topTextHandler(){
+        when (g1l2ViewModel.gameState.value){
+            0, 1, 2 -> {
+                binding.g1l2TopTextView.text = getString(R.string.g1l2_code_length,
+                    g1l2ViewModel.lengthToBeShown)
+            }
+            3 -> {
+                binding.g1l2TopTextView.text = getString(R.string.g1l2_code_length,
+                    g1l2ViewModel.lengthToBeShown)
+            }
+            else -> {
+                binding.g1l2TopTextView.text = ""
+            }
+        }
+    }
+
+    private fun middleTextHandler(){
+        when (g1l2ViewModel.gameState.value){
+            1 -> {
+                binding.g1l2SequenceText.text = g1l2ViewModel.code.value.toString()
+            }
+            2 -> {
+                binding.g1l2SequenceText.text = ""
+            }
+            4 -> {
+                binding.g1l2SequenceText.text = getString(R.string.g1l2_incorrect, g1l2ViewModel.code.value)
+            }
+            5 -> {
+                binding.g1l2SequenceText.text = getString(R.string.g1l2_correct)
+            }
+            else -> {
+                binding.g1l2SequenceText.text = ""
+            }
+
+        }
+    }
+
+    private fun bottomTextHandler(){
+        when (g1l2ViewModel.gameState.value){
+            0, 1, 2, 3, 4, 5 -> {
+                binding.g1l2BottomTextView.text = getString(R.string.g1l2_info)
+            }
+            else -> {
+                binding.g1l2BottomTextView.text = ""
+            }
+        }
+    }
+
+
+    private fun buttonIconHandler(){
+        when (g1l2ViewModel.gameState.value){
+            0 -> {
+                binding.g1l2StartButton.setImageResource(R.drawable.ic_baseline_touch)
+            }
+            else -> {
+                binding.g1l2StartButton.setImageDrawable(null)
+            }
+        }
+    }
+
+    private fun progressBarHandler(){
+        binding.g1l2ProgressBar.progress = g1l2ViewModel.progressBar.value!!
+    }
+
+
+    private fun layoutVisibilityHandler(){
+        when (g1l2ViewModel.gameState.value){
+            0 -> {
+                binding.g1l2StartLayout.visibility = View.VISIBLE
+                binding.g1l2SequenceLayout.visibility = View.INVISIBLE
+                binding.g1l2ProgressLayout.visibility = View.INVISIBLE
+                binding.g1l2InputLayout.visibility = View.INVISIBLE
+            }
+            1 -> {
+                binding.g1l2StartLayout.visibility = View.INVISIBLE
+                binding.g1l2SequenceLayout.visibility = View.VISIBLE
+                binding.g1l2ProgressLayout.visibility = View.INVISIBLE
+                binding.g1l2InputLayout.visibility = View.INVISIBLE
+            }
+            2 -> {
+                binding.g1l2StartLayout.visibility = View.INVISIBLE
+                binding.g1l2SequenceLayout.visibility = View.INVISIBLE
+                binding.g1l2ProgressLayout.visibility = View.VISIBLE
+                binding.g1l2InputLayout.visibility = View.INVISIBLE
+            }
+            3 -> {
+                binding.g1l2StartLayout.visibility = View.INVISIBLE
+                binding.g1l2SequenceLayout.visibility = View.INVISIBLE
+                binding.g1l2ProgressLayout.visibility = View.INVISIBLE
+                binding.g1l2InputLayout.visibility = View.VISIBLE
+            }
+            4 -> {
+                binding.g1l2StartLayout.visibility = View.INVISIBLE
+                binding.g1l2SequenceLayout.visibility = View.VISIBLE
+                binding.g1l2ProgressLayout.visibility = View.INVISIBLE
+                binding.g1l2InputLayout.visibility = View.INVISIBLE
+            }
+            5 -> {
+                binding.g1l2StartLayout.visibility = View.INVISIBLE
+                binding.g1l2SequenceLayout.visibility = View.VISIBLE
+                binding.g1l2ProgressLayout.visibility = View.INVISIBLE
+                binding.g1l2InputLayout.visibility = View.INVISIBLE
+            }
+        }
+    }
+
+    private fun openScoreFragment(){
+        if (openScore.value == true) {
+            navController!!.navigate(R.id.action_game1l2Fragment_to_game1l2ScoreFragment)
+        }
+    }
+
+
+
+}
---
+++ app/src/main/java/com/narrowstudio/bigbrainz/viewmodel/game1/Game1l3ViewModel.kt
@@ -0,0 +1,226 @@
+package com.narrowstudio.bigbrainz.viewmodel.game1
+
+import android.os.Handler
+import android.os.Looper
+import android.util.Log
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
+import com.narrowstudio.bigbrainz.data.G1DBEntry
+import com.narrowstudio.bigbrainz.data.G1Dao
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.*
+import javax.inject.Inject
+import kotlin.math.pow
+import kotlin.math.roundToInt
+import kotlin.random.Random
+
+@HiltViewModel
+class Game1l3ViewModel @Inject constructor(
+    private val g1Dao: G1Dao
+) : ViewModel() {
+
+    /**    ---------------------- gameState ----------------------------------
+     *      0 - not running
+     *      1 - displaying the code
+     *      2 - displaying intercolor
+     *      3 - reading the input
+     *      4 - displaying wrong
+     *      5 - displaying correct
+     */
+    val gameState: MutableLiveData<Int> = MutableLiveData()
+
+    // time of starting the timer in millis
+    private var startTime: Long = 0
+
+    // random code shown
+    val code: MutableLiveData<Long> = MutableLiveData()
+
+
+    // sends info to fragment to open score fragment
+    var openScore: MutableLiveData<Boolean> = MutableLiveData()
+
+    // progress bar filler
+    val progressBar: MutableLiveData<Int> = MutableLiveData()
+
+    // length of the first challenge
+    private val lengthAtTheBeginning = 3
+    // length of level
+    var lengthToBeShown = lengthAtTheBeginning
+    // increment of length at each level
+    private val levelUp = 1
+
+    // time for the code in millis
+    private val displayTime = 2000
+
+    // time for intercolor in millis
+    private val interColorTIme = 1500
+
+    // time of displaying wrong message
+    private val wrongTime = 2000
+
+    // time of displaying correct message
+    private val correctTime = 2000
+
+    val saves = g1Dao.getEntries().asLiveData()
+
+
+    private var handler = Handler(Looper.getMainLooper())
+
+    private val scope = CoroutineScope(Dispatchers.Main)
+
+    private var runnable: java.lang.Runnable = object: java.lang.Runnable {
+        override fun run(){
+            when (gameState.value){
+                1 -> {
+                    if (System.currentTimeMillis() >= startTime + displayTime){
+                        codeToInter()
+                    }
+                }
+                2 -> {
+                    if (System.currentTimeMillis() >= startTime + interColorTIme){
+                        interToInput()
+                    } else {
+                        progressBar.postValue(progressBarCalc(System.currentTimeMillis() - startTime))
+                    }
+                }
+                4 -> {
+                    if (System.currentTimeMillis() >= startTime + wrongTime){
+                        wrongToStart()
+                    }
+                }
+                5 -> {
+                    if (System.currentTimeMillis() >= startTime + correctTime){
+                        correctToStart()
+                    }
+                }
+            }
+
+            handler.postDelayed(this, 1)
+        }
+    }
+
+
+
+
+    fun init(){
+        stopTimer()
+        lengthToBeShown = lengthAtTheBeginning
+        openScore.postValue(false)
+        progressBar.postValue(0)
+        gameState.postValue(0)
+    }
+
+    fun mainButtonPressed(){
+        when (gameState.value){
+            0 -> {
+                startGame()
+            }
+            else -> {
+                // do nothing
+            }
+        }
+    }
+
+    fun codeEntered(codeEntered: Long){
+        if (codeEntered == code.value){
+            inputToCorrect()
+        } else {
+            inputToWrong()
+        }
+    }
+
+
+    private fun startGame(){
+        Log.d("Game status", "Game started")
+        code.postValue(randomizeCode())
+        gameState.postValue(1)
+        startTime = System.currentTimeMillis()
+        startTimer()
+    }
+
+    private fun restartGame(){
+        // if any successful pattern, save
+        if (lengthToBeShown == lengthAtTheBeginning) {
+            gameState.postValue(4)
+            lengthToBeShown = lengthAtTheBeginning
+            startTime = System.currentTimeMillis()
+            startTimer()
+        } else {
+            // if any successful pattern, open score
+            insertNewEntry()
+            openScore.postValue(true)
+        }
+    }
+
+    private fun nextLevel(){
+        lengthToBeShown += levelUp
+        Log.d("Game status", "Level up. Current level: $lengthToBeShown")
+        gameState.postValue(5)
+        startTime = System.currentTimeMillis()
+        startTimer()
+    }
+
+    // transition from state 1 to 2
+    private fun codeToInter(){
+        gameState.postValue(2)
+        startTime = System.currentTimeMillis()
+        startTimer()
+    }
+
+    // transition from state 2 to 3
+    private fun interToInput(){
+        stopTimer()
+        gameState.postValue(3)
+    }
+
+    // transition from 3 to 4
+    private fun inputToWrong(){
+        restartGame()
+    }
+
+    // transition from 3 to 5
+    private fun inputToCorrect(){
+        nextLevel()
+    }
+
+    // transition from 4 to 0
+    private fun wrongToStart(){
+        init()
+    }
+
+    // transition from 5 to 0
+    private fun correctToStart(){
+        stopTimer()
+        gameState.postValue(0)
+    }
+
+
+
+    private fun randomizeCode(): Long{
+        return (10.toDouble().pow(lengthToBeShown -1).toLong() .. 10.toDouble().pow(lengthToBeShown).toLong()).random(Random(System.currentTimeMillis()))
+    }
+
+    // returning percentage for progressBar
+    private fun progressBarCalc(delta: Long): Int {
+        return ((delta.toFloat() / interColorTIme.toFloat())*100).roundToInt()
+    }
+
+    private fun startTimer(){
+        handler.removeCallbacks(runnable)
+        handler.postDelayed(runnable, 1)
+    }
+
+    private fun stopTimer(){
+        handler.removeCallbacks(runnable)
+        gameState.postValue(0)
+    }
+
+    //-------------------------------------------------- DB
+    private fun insertNewEntry() {
+        scope.launch {
+            g1Dao.insert(G1DBEntry(102, lengthToBeShown - 1))
+        }
+    }
+
+}
